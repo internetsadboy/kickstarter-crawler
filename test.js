92cd15c71bc6fc58d6d2611d76bba803dbc87365
ghostsnstuff <jaredhalpert90@gmail.com>
2014-03-04 00:37:55 -0800

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var ks = require('./ks')
   , options = {url: 'http://www.kickstarter.com/projects/maxtemkin/philosophy-posters'}
   , project = new ks.project(options);
 
-// fetch location, funding, general, and time
+// fetch -> print: location, funding, general, and time
 project.getLocation().getFunding().getGeneral().getTime(function(err, data) {
   if(err) throw err;
   console.log(data);
