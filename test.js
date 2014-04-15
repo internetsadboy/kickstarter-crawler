3c08bd475d480aec088cb23c1a49326d950c7cca
jared <jaredhalpert90@gmail.com>
2014-04-15 07:54:07 -0700

    eh

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var ks = require('./ks');
 var options = {
 	url:'http://www.kickstarter.com/projects/maxtemkin/philosophy-posters',
@@ -11,14 +10,3 @@ project.request(function(err, data) {
 	if(err) throw err;
 	console.log(data);
 })
-=======
-var ks = require('./ks')
-  , options = {url: 'http://www.kickstarter.com/projects/maxtemkin/philosophy-posters'}
-  , project = new ks.project(options);
-
-// fetch -> print: location, funding, general, and time
-project.getLocation().getFunding().getGeneral().getTime(function(err, data) {
-  if(err) throw err;
-  console.log(data);
-});
->>>>>>> d7254ca29daacd5217104fb995e873904cb6f28d
