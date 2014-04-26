429ed6b18e870091629a292bd1ffc7df45f31f7f
ghostsnstuff <jaredhalpert90@gmail.com>
2014-04-25 19:40:39 -0700

    Update log.js

--- tests/log.js
+++ tests/log.js
@@ -1,9 +1,9 @@
 var ks = require('../ks');
 var project = new ks.project({
-	url:'https://www.kickstarter.com/projects/597507018/pebble-e-paper-watch-for-iphone-and-android',
-	fields:['general','time','funding','location','other','facebook','media','pledges'],
-	log:true
+  url:'https://www.kickstarter.com/projects/597507018/pebble-e-paper-watch-for-iphone-and-android',
+  fields:['general','time','funding','location','other','facebook','media','pledges'],
+  log:true
 });
 project.request(function(err, data) {
-	if(err) throw err;
+  if(err) throw err;
 });
