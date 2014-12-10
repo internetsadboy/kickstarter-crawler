e0dc2ffda8b547f0c5e9e5c9f5cec27801cab683
Jared Halpert <jaredhalpert90@gmail.com>
2014-12-09 21:37:02 -0800

    refactored: updated parse method example

--- examples/parse.js
+++ examples/parse.js
@@ -1,14 +1,32 @@
-var uri = 'https://www.kickstarter.com/projects/597507018/pebble-e-paper-watch-for-iphone-and-android';
-var request = require('request');
-var ks = require('../ks');
+'use strict';
+
+var KS_CRAWLER = require('../ks'),
+    request = require('request');
 
 
-// crawl kickstarter, then parse data separately
-request(uri, function(err, res, body) {
-  if(err) {
-    console.error(new Error('something went wrong'));
+var url = 'https://www.kickstarter.com/projects/597507018/pebble-e-paper-watch-for-iphone-and-android';
+
+
+/**
+ * Make a HTTP request to the kickstarter project url and parse the HTML
+ * @param  {String}    url
+ * @param  {Function}  onRequest
+ */
+request(url, function onRequest (err, res, body) {
+  var project, data;
+
+  if (err) {
+    console.error('Bad request');
   }
-  var project = new ks.project();
-  var data = project.parse(body);
+
+  // create an instance of the kickstarter project crawler
+  project = new KS_CRAWLER.project({
+    url: url
+  });
+
+  // parse HTML
+  data = project.parse(body);
+
+  // log the project's successfully parsed data points
   console.log(data.successes);
 });
