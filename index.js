5b1f816d934ceae2275df1c934ee0b0638c88dab
Jared Lamont <jaredlamont90@gmail.como>
2020-07-27 14:13:07 -0700

    2020 UPDATE

--- index.js
+++ index.js
@@ -1,1 +1,109 @@
-module.exports.project = require('./lib/project');
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+
+module.exports = async function kickstarterCrawler(url) {
+  try {
+    let res = await axios.get(url);
+
+    if (res.data) {
+      let data = parseHTML(res);
+      return data;
+    }
+  } catch(err) {
+    console.error(err);
+  }
+}
+
+function parseHTML(res) {
+  const data = {};
+
+  // parse html ie "load"
+  const $ = cheerio.load(res.data);
+
+  const was_cancelled = $('#main_content').hasClass('Campaign-state-canceled');
+
+  const title =
+    $('.project-profile__title')
+      .eq(0)
+      .text()
+      .trim();
+
+  const creator =
+    $('.hero__link')
+      .eq(1)
+      .text()
+      .trim();
+
+  const description =
+    $('.project-profile__blurb')
+      .text()
+      .trim();
+
+  // Sometimes kickstarter has a "projects we love" section before category/location
+  const has_flag = $('.NS_projects__category_location').children().length === 3
+  const category_location_offset = has_flag ? 1 : 0;
+
+  // leave unstructured (ie US vs international formatting)
+  const location =
+    $('.NS_projects__category_location a')
+      .eq(category_location_offset)
+      .text()
+      .trim()
+      .split(', ');
+
+  const category =
+    $('.NS_projects__category_location a')
+      .eq(category_location_offset + 1)
+      .text()
+      .trim();
+
+  const duration =
+    $('.NS_campaigns__funding_period .f5')
+      .text()
+      .trim()
+      .split('\n')[1]
+      .replace(/[^0-9]/g, '');
+
+  const startdate =
+    $('.NS_campaigns__funding_period time')
+      .eq(0)
+      .text()
+      .trim();
+
+  const enddate =
+    $('.NS_campaigns__funding_period time')
+      .eq(1)
+      .text()
+      .trim();
+
+  const funding =
+    $('.money')
+    .eq(1)
+    .text();
+
+  const goal =
+    $('.money')
+      .eq(2)
+      .text();
+
+  const backers =
+    $('h3.mb0')
+      .text()
+      .trim()
+      .split('\n')[2];
+
+  data.title = title;
+  data.creator = creator;
+  data.description = description;
+  data.category = category;
+  data.location = location;
+  data.duration = duration;
+  data.startdate = startdate;
+  data.enddate = enddate;
+  data.funding = funding;
+  data.goal = goal;
+  data.backers = backers;
+
+  return data;
+}
